d463aaf43134e2705496f6a5fb053c160f1bc998
frep <nickfrappier1217@gmail.com>
2020-07-03 17:08:57 -0400

    Speed I

--- src/me/frep/thotpatrol/checks/movement/speed/SpeedI.java
+++ src/me/frep/thotpatrol/checks/movement/speed/SpeedI.java
@@ -5,12 +5,16 @@ import me.frep.thotpatrol.checks.movement.ascension.AscensionA;
 import me.frep.thotpatrol.events.SharedEvents;
 import me.frep.thotpatrol.utils.*;
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +23,7 @@ import java.util.UUID;
 public class SpeedI extends Check {
 
     private Map<UUID, Integer> verbose = new HashMap<>();
-    private Map<UUID, Long> onStairs = new HashMap<>();
+    private Map<UUID, Long> invalidBlock = new HashMap<>();
     private Map<UUID, Long> belowBlock = new HashMap<>();
 
     public SpeedI(me.frep.thotpatrol.ThotPatrol ThotPatrol) {
@@ -32,10 +36,12 @@ public class SpeedI extends Check {
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
-        double delta = UtilMath.getHorizontalDistance(e.getFrom(), e.getTo());
+        double delta = UtilMath.offset(getHV(e.getTo().toVector()), getHV(e.getFrom().toVector()));
         double maxDelta = .35;
-        if (UtilBlock.isStair(p.getLocation().clone().subtract(0, 1, 0).getBlock())) {
-            onStairs.put(p.getUniqueId(), System.currentTimeMillis());
+        if (UtilBlock.isStair(p.getLocation().clone().subtract(0, 1, 0).getBlock())
+            || (p.getLocation().subtract(0, 1, 0).getBlock().getType().toString().contains("SLIME")
+            || p.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().toString().contains("SLIME"))) {
+            invalidBlock.put(p.getUniqueId(), System.currentTimeMillis());
         }
         if (UtilBlock.isSolid(p.getEyeLocation().clone().add(0, 1, 0).getBlock())
             || UtilBlock.isSolid(p.getEyeLocation().clone().add(0, 2, 0).getBlock())
@@ -49,13 +55,14 @@ public class SpeedI extends Check {
             || p.getAllowFlight()
             || p.getVehicle() != null
             || e.isCancelled()
+            || p.getLocation().getBlock().isLiquid()
             || p.hasPermission("thotpatrol.bypass")
             || SpeedC.jumpingOnIce.contains(p.getUniqueId())
             || UtilPlayer.isOnClimbable(p, 0)
             || UtilPlayer.isOnClimbable(p, 1)
             || UtilPlayer.isOnClimbable(p)
             || !UtilTime.elapsed(belowBlock.getOrDefault(p.getUniqueId(), 0L), 1000L)
-            || !UtilTime.elapsed(onStairs.getOrDefault(p.getUniqueId(), 0L), 1000L)
+            || !UtilTime.elapsed(invalidBlock.getOrDefault(p.getUniqueId(), 0L), 1000L)
             || !UtilTime.elapsed(SharedEvents.getLastJoin().getOrDefault(p.getUniqueId(), 0L), 1500)
             || !UtilTime.elapsed(AscensionA.toggleFlight.getOrDefault(p.getUniqueId(), 0L), 5000L)
             || !UtilTime.elapsed(getThotPatrol().LastVelocity.getOrDefault(p.getUniqueId(), 0L), 2000)) {
@@ -64,15 +71,29 @@ public class SpeedI extends Check {
         if (p.getMaximumNoDamageTicks() < 15) {
             maxDelta += .04;
         }
+        //todo
+        if (p.getWalkSpeed() > .21) {
+            maxDelta += p.getWalkSpeed() * 1;
+        }
+        for (PotionEffect effect : p.getActivePotionEffects()) {
+            if (effect.getType().equals(PotionEffectType.SPEED)) {
+                maxDelta += (effect.getAmplifier() + 1) * .05;
+            }
+        }
         int count = verbose.getOrDefault(p.getUniqueId(), 0);
         if (delta > maxDelta) {
             count++;
-            Bukkit.broadcastMessage("count increase " + p.getName());
+            Bukkit.broadcastMessage("count increase " + p.getName() + " delta=" + delta);
         }
         if (count > 3) {
             count = 0;
             getThotPatrol().logCheat(this, p, delta + " > .35");
         }
         verbose.put(p.getUniqueId(), count);
+    }
+
+    private Vector getHV(Vector V) {
+        V.setY(0);
+        return V;
     }
 }
